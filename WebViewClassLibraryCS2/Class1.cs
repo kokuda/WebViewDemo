06714f0a90d942e26c6e8a8bacee6fe761817895
Kaz Okuda 🎃 <kaokuda@microsoft.com>
2023-05-13 00:57:15 -0700

    Added IList<int> GetNumbers() and new class Thing and IList<Thing> getThings() and tests around them

--- WebViewClassLibraryCS2/Class1.cs
+++ WebViewClassLibraryCS2/Class1.cs
@@ -2,6 +2,17 @@
 
 namespace WebViewClassLibraryCS2
 {
+    sealed public class Thing
+    {
+        public Thing()
+        {
+
+        }
+
+        public string prop { get => "Thing string";  }
+
+    }
+
     sealed public class Class1
     {
         public Class1()
@@ -11,6 +22,16 @@ namespace WebViewClassLibraryCS2
         public void RunMe()
         {
             Debug.WriteLine("Hello from WebViewClassLibraryCS2");
+        }
+
+        public IList<string> GetNumbers()
+        {
+            return new List<string>() { "A", "B", "C" };
+        }
+
+        public IList<Thing> GetThings()
+        {
+            return new List<Thing>() { new Thing() };
         }
     }
 }
